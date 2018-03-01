e8fec1cc3066f023a0516dd6c9425c8749809017
Manuel Herrera Ojea <manuelherreraojea@cvi092081.ugr.es>
2018-03-01 20:12:59 +0100

    Ride mejorada

--- point.h
+++ point.h
@@ -3,12 +3,15 @@
 #define _POINT_
 
 class point {
-	int x;
-	int y;
 
 	public:
 
+		int x;
+		int y;
+
 		point( int i = 0, int j = 0 ) : x(i), y(j) {}
+
+		point( point p ) : x(p.x), y(p.y);
 };
 
 #endif
--- ride.h
+++ ride.h
@@ -1,10 +1,40 @@
 
+#ifndef _RIDE_
+#define _RIDE_
+
+#include "point.h"
+
 
 class Ride {
 
-	int something;
+	bool assigned;
+	point start, finish;
+	int startTime, finishTime;
 
 	public:
 
-		Ride() { something = 0; }
+		Ride( point start, point finish, int startTime, int finishTime ){
+
+			this->start = start;
+			this->finish = finish;
+			this->startTime = startTime;
+			this->finishTime = finishTime;
+		}
+
+		Ride( Ride r ){
+			start = r.start;
+			finish = r.finish;
+			startTime = r.startTime;
+			finishTime = r.finishTime;
+		}
+
+		point GetStart(){
+			return start;
+		}
+
+		point GetFinish(){
+			return finish;
+		}
 };
+
+#endif
