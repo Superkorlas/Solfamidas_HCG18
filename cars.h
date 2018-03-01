2c6476606166c882bb8dc2775cf76d068a0fb76b
Guillermo Bueno Vargas <Guillergood@hotmail.com>
2018-03-01 20:04:47 +0100

    Updated cars.h
    various methods

--- cars.h
+++ cars.h
@@ -2,17 +2,29 @@
 #define _CARS_
 #include "point.h"
 #include "car.h"
+#include "rides.h"
+#include "ride.h"
 
 class Cars {
     std::vector<Car> cars;
+    Rides rides;
 
     Cars(int numberCars){
         cars.resize(numberCars);
     }
 
 
-    Point getCarPosition(int carIndex){
+    Point Cars::GetCarPosition(int carIndex){
         cars[carIndex].getPosition();
+    }
+
+    void Cars::Update(){
+        for (int i = 0; i < cars.size(); i++){
+            if(cars[i].GetBusy() == false){
+                //TODO 
+                cars[i].SetRide();
+            }
+        }
     }
 
 
